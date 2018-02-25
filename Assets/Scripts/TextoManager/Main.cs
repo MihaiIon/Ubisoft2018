9192e24f954a2b8a81a9b4a0af8fadd77e563fcb
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2018-02-25 12:56:08 -0500

    First commit

---
+++ Assets/Scripts/TextoManager/Main.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextoManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
