0c20b885703bab605ff17b20a9f2797e23080068
Mihai Ionescu <mihai.t.ionescu@gmail.com>
2018-02-25 22:19:10 -0500

    Added GameManager scripts and manager

---
+++ Assets/Scripts/Managers/GameManager/InGameController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameController : MonoBehaviour {
+
+    // GameManager reference
+    private GameManager gameManager;
+    public InGameManager inGameManager;
+
+    private void Awake()
+    {
+        gameManager = GetComponent<GameManager>();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+	public void Init ()
+    {
+
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public void Dismiss()
+    {
+
+    }
+}
