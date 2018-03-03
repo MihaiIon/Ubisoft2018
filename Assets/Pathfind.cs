8154470c558f194d473512b2b24234b311b7ee86
Francis de Ladurantaye <fdl9044@gmail.com>
2018-03-02 20:17:26 -0500

    pathfinding
    ajout du pathfinding modulaire

---
+++ Assets/Pathfind.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfind : MonoBehaviour {
+
+    // private GameObjet go;
+
+    public Transform goal;
+    private UnityEngine.AI.NavMeshAgent agent;
+
+    // Use this for initialization
+    void Start () {
+
+        // this.goal = goal;
+        // agent = go.AddComponent<UnityEngine.AI.NavMeshAgent>();
+
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        agent.destination = goal.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        agent.destination = goal.position;
+	}
+}
